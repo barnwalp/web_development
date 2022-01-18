378bcdedf3739d836f2e4e7ba5076dd87ee2e3a9
Pankaj Barnwal <suno.pankaj@gmail.com>
2022-01-18 08:22:05 +0530

    feat/adding various google tiles over leaflet

--- ip_address_tracker/app/js/script.js
+++ ip_address_tracker/app/js/script.js
@@ -7,7 +7,6 @@ var map = L.map('map').setView([21.46, 83.97], 13);
 var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
-osm.addTo(map);
 
 //water color
 var waterColor = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
@@ -17,4 +16,31 @@ var waterColor = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercol
 	maxZoom: 16,
 	ext: 'jpg'
 });
-// waterColor.addTo(map);
+
+//google street map
+var googleStreets = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',{
+    maxZoom: 20,
+    subdomains:['mt0','mt1','mt2','mt3']
+});
+
+//google hybrid map
+
+googleHybrid = L.tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}',{
+    maxZoom: 20,
+    subdomains:['mt0','mt1','mt2','mt3']
+});
+
+//google satellite map
+googleSat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{
+    maxZoom: 20,
+    subdomains:['mt0','mt1','mt2','mt3']
+});
+
+//google terrain map
+googleTerrain = L.tileLayer('http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}',{
+    maxZoom: 20,
+    subdomains:['mt0','mt1','mt2','mt3']
+});
+
+//adding tile to our map
+googleStreets.addTo(map);
