fab73109de9458d9d78b93d20da388000b4cad77
Pankaj Barnwal <suno.pankaj@gmail.com>
2022-01-18 08:13:37 +0530

    feat/adding water layer over osm layer in leaflet

--- ip_address_tracker/app/js/script.js
+++ ip_address_tracker/app/js/script.js
@@ -1,9 +1,20 @@
 //21.467296481745876, 83.97723937273383
 
+//Map Initialization
 var map = L.map('map').setView([21.46, 83.97], 13);
 
+//osm layer
 var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
-
 osm.addTo(map);
+
+//water color
+var waterColor = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	subdomains: 'abcd',
+	minZoom: 1,
+	maxZoom: 16,
+	ext: 'jpg'
+});
+// waterColor.addTo(map);
