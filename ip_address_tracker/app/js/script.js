2c9e97231195466d3d74e6a7cd26f003053cb45b
Pankaj Barnwal <suno.pankaj@gmail.com>
2022-01-18 07:38:19 +0530

    feat/resolving leaflet map issue

--- ip_address_tracker/app/js/script.js
+++ ip_address_tracker/app/js/script.js
@@ -1,1 +1,9 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+//21.467296481745876, 83.97723937273383
+
+var map = L.map('map').setView([21.46, 83.97], 13);
+
+var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+
+osm.addTo(map);
