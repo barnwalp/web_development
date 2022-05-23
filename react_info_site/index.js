da62881f37bb4e4b78a2c78851e0a6348dd61cb9
Pankaj Barnwal <suno.pankaj@gmail.com>
2022-05-23 08:27:35 +0530

    feat/adding navbar in react using cdns

--- react_info_site/index.js
+++ react_info_site/index.js
@@ -1,14 +1,18 @@
-console.log('test');
-
-class App extends React.Component {
-  render() {
-    return (
-      <div className="app-content">
-        <h1>Hello world</h1>
-      </div>
-    )
-  }
+// only PascalCase is supported as a funciton nav in React
+const NavBar = () => {
+  return (
+    <nav>
+      <h1>website</h1>
+      <ul>
+        <li>Pricing</li>
+        <li>About</li>
+        <li>Contract</li>
+      </ul>
+    </nav>
+  )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
-// ReactDOM.render(<h1>Rendering from ReactDom</h1>, document.getElementById('root'));
+ReactDOM.render(
+  <NavBar />,
+  document.getElementById('root')
+);
