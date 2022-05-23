29c3b51ffa7753722058f8425d250fdde4a5f05a
Pankaj Barnwal <suno.pankaj@gmail.com>
2022-05-23 08:38:52 +0530

    feat/adding Hero function to the render method

--- react_info_site/index.js
+++ react_info_site/index.js
@@ -1,3 +1,5 @@
+const root = document.getElementById('root');
+
 // only PascalCase is supported as a funciton nav in React
 const NavBar = () => {
   return (
@@ -12,7 +14,25 @@ const NavBar = () => {
   )
 }
 
+let hero_section = (
+  <div className="hero-container">
+    <h3>Hero Section</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit, accusantium molestiae soluta repellat quia nobis dolore laboriosam maiores reprehenderit sapiente quos eum, temporibus natus doloremque perspiciatis, quaerat et beatae corrupti.</p>
+  </div>
+)
+
+const Hero = () => {
+  return hero_section;
+}
+
+// hero_section is actually an object which is returned by JSX
+// and render method uses this object to create the DOM
+console.log(hero_section);
+
 ReactDOM.render(
-  <NavBar />,
-  document.getElementById('root')
+  <div>
+    <NavBar />
+    <Hero />
+  </div>,
+  root
 );
