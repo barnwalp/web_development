4d8942897b93544ecbe5dbc9d18c87c54685937f
Pankaj Barnwal <suno.pankaj@gmail.com>
2022-05-24 07:58:31 +0530

    feat/creating components in react App

--- react-fact/src/index.js
+++ react-fact/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './App.scss';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  // strictmode is a tool for highlighting potential problem
   <React.StrictMode>
     <App />
   </React.StrictMode>
@@ -14,4 +16,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(App);
