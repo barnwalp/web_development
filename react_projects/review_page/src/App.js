c4a8b42e05a2a2286a47e08fd3da9b98eb49b090
Pankaj Barnwal <suno.pankaj@gmail.com>
2022-05-25 21:45:59 +0530

    feat/getting photos from unsplash api

--- react_projects/review_page/src/App.js
+++ react_projects/review_page/src/App.js
@@ -1,6 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
+const loadData = async () => {
+  let unsplash;
+  let url = "https://api.unsplash.com/search/photos?";
+  let key = "IGlo6mau3iyLYRAHuWtm8D0eCV1CLrjasdbj0tS1Qqk";
+  let query = "woman";
+  url = url + 'client_id=' + key + '&query=' + query;
+  try {
+    const res = await fetch(url);
+    unsplash = await res.json();
+    console.log(unsplash);
+    return unsplash;
+  } catch(err) {
+    console.log(err);
+  }
+}
+loadData();
+
 function App() {
   return (
     <div className="App">
