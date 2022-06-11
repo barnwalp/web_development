f21c9d07c0011d8bfe8c047243e832a08000cad4
Pankaj Barnwal <suno.pankaj@gmail.com>
2022-06-11 07:59:24 +0530

    feat/adding states to review page

--- react_projects/review_page/src/App.js
+++ react_projects/review_page/src/App.js
@@ -10,44 +10,44 @@ class App extends React.Component {
 		};
 	}
 
-	async loadData() {
-		let link=[];
-		let user=[];
-		let unsplashPics;
-		let url = "https://api.unsplash.com/search/photos?";
-		let key = "IGlo6mau3iyLYRAHuWtm8D0eCV1CLrjasdbj0tS1Qqk";
-		let query = "woman";
-		url = url + 'client_id=' + key + '&query=' + query;
-		const res = await fetch(url);
-		if(res.ok) {
-			unsplashPics = await res.json();
-			for (let i=0; i<3; i++){
-				link.push(unsplashPics.results[i].urls.full);
-				user.push(unsplashPics.results[i].user.username);
+	loadData() {
+		console.log('loadData is being run');
+		const fetchUserData = async () => {
+			let link=[];
+			let user=[];
+			let unsplashPics;
+			let url = "https://api.unsplash.com/search/photos?";
+			let key = "IGlo6mau3iyLYRAHuWtm8D0eCV1CLrjasdbj0tS1Qqk";
+			let query = "woman";
+			url = url + 'client_id=' + key + '&query=' + query;
+			const res = await fetch(url);
+			if(res.ok) {
+				console.log('response is ok');
+				for (let i=0; i<3; i++) {
+					link.push(unsplashPics.results[i].urls.full);
+					user.push(unsplashPics.results[i].user.username);
+				}
+				this.setState({
+					imgUrl: link,
+					imgUser: user,
+				})
+				console.log('checking imgUrl in state:');
+				console.log(this.state.imgUser);
+			} else {
+				console.log('could not get data: ', + res.status);
 			}
-		this.setState({
-				imgUrl: link,
-				imgUser: user,
-			})
-		} else {
-			console.log('could not get data: ', + res.status);
 		}
+		fetchUserData();
 	}
-}
 
-function Items() {
-	const a = new App();
-	const pics = a.loadData();
-	pics.then((data) => {
+	render() {
 		return (
 			<div className="container">
 				<div className="item">
-					<img src={data.results[0].urls.full} alt="woman" />
-					<p>{data.results[0].user.username}</p>
 				</div>
 			</div>
 		)
-	});
+	}
 }
 
-export default Items;
+export default App;
