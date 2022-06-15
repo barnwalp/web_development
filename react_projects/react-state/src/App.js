925e0eb7c6653d62e138e70f37f1a31d2bf63195
Pankaj Barnwal <suno.pankaj@gmail.com>
2022-06-15 07:49:11 +0530

    feat/using click handler to change state

--- react_projects/react-state/src/App.js
+++ react_projects/react-state/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 function App() {
+	/*
 	// declaring a state
 	// useState returns an array
 	const [result, func] = React.useState('Yes');
@@ -8,10 +9,24 @@ function App() {
 
 	//to change the state value, func is used
 	func('No');
+	*/
+
+	// Better way to manage state
+	const [isImportant, setIsImportant] = React.useState("Yes");
+
+	function handleClick() {
+		if(isImportant === "Yes") {
+			setIsImportant("No");
+		} else {
+			setIsImportant("Yes");
+		}
+	}
+
   return (
     <div className="App">
 			<h1>Is React state important?</h1>
-			<p>{result}</p>
+			<p>{isImportant}</p>
+			<button onClick={handleClick} className="btn">Toggle</button>
     </div>
   )
 }
