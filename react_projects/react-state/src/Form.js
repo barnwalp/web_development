09d2b3b9179682eaacb45a69aa5312b08d4a2aa8
Pankaj Barnwal <suno.pankaj@gmail.com>
2022-06-19 08:18:32 +0530

    feat/checkbox and textarea in react forms

--- react_projects/react-state/src/Form.js
+++ react_projects/react-state/src/Form.js
@@ -5,24 +5,68 @@ function Form() {
 	const [formData, setFormData] = React.useState({
 		firstName: "",
 		lastName: "",
+		email: "",
+		comment: "",
+		isFriendly: true,
 	})
 	
 	console.log(formData);
 
 	function handleInput(event) {
+		const {name, value, type, checked} = event.target;
 		setFormData(currData => {
 			return({
 				...currData,
-				[event.target.name]: event.target.value
+				[name]: (type === "input") ? value : checked
 			})
 		})
 	}
 
 	return (
 		<div className="form">
-			<input className="input" onChange={handleInput} type="text" name="firstName" placeholder="First Name" />
-			<input className="input" onChange={handleInput} type="text" name="lastName" placeholder="Last Name" />
-			// <input className="btn" onChange={handleInput} type="submit" />
+			<input 
+				className="input" 
+				onChange={handleInput} 
+				type="text"
+				name="firstName" 
+				placeholder="First Name" 
+				// Conceptually it makes no difference, but it ensures that
+				// input tag is not maintaining its own state but relying on
+				// react for the data
+				value={formData.firstName}
+			/>
+			<input 
+				className="input" 
+				onChange={handleInput} 
+				type="text" 
+				name="lastName" 
+				placeholder="Last Name" 
+				value={formData.lastName}
+			/>
+			<input 
+				className="input" 
+				onChange={handleInput} 
+				type="text" 
+				name="email" 
+				placeholder="Email" 
+				value={formData.email}
+			/>
+			<textarea 
+				name="comment" 
+				onChange={handleInput}
+				value={formData.comment} 
+				placeholder="Comment"
+				cols="30" 
+				rows="10" 
+			/>
+			<input 
+				name="isFriendly"
+				type="checkbox" 
+				checked={formData.isFriendly}
+				id="isFriendly"
+				onChange={handleInput}
+			/>
+			<label htmlFor="isFriendly">Are you friendly?</label>
 		</div>
 	)
 }
