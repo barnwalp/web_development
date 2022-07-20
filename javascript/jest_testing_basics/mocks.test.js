f278ce8ba2f808f3332366525b9ae71ea346ce12
Pankaj Barnwal <suno.pankaj@gmail.com>
2022-07-20 21:40:06 +0530

    feat/mocks in jest unit testing

---
+++ javascript/jest_testing_basics/mocks.test.js
@@ -0,0 +1,13 @@
+const forEach = (items, callback) => {
+	for (let i=0; i<items.length; i++) {
+		callback(items[i]);
+	}
+}
+
+it ('mock callback', () => {
+	const mockCallback = jest.fn(x => 42 + x);
+	forEach([0, 1], mockCallback);
+
+	// Each callback has a mock property
+	expect(mockCallback.mock.calls.length).toBe(2)
+})
