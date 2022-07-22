bcddb9ad1671ad76707c1e986f8ba47fa2afeb9f
Pankaj Barnwal <suno.pankaj@gmail.com>
2022-07-22 22:28:40 +0530

    feat/changing mocking implementation mid test

--- javascript/jest_testing_basics/__tests__/isAtLeast18.test.js
+++ javascript/jest_testing_basics/__tests__/isAtLeast18.test.js
@@ -1,20 +1,37 @@
 /*
-	* Using mocks, one can control the return of a function dependency, not matter what arguments it is called with.
-	* Mock functions help manipulate the control flow of the tested program and reach even those difficult to reproduce edge cases when writing test.
+	* A Mock function is a function that replaces the actual implementation of a function with a fake (mock) implementation.
+	* With a mock function, we can know the number of times the function was called, the argument it was called with, the result it returned and more. this ability to spy on function calls is why mock functions are also called spies.
 */
 
 // In this scenario, we have two functions, isInteger and isAtLeast18, in which isAtLeast18 is dependent on isInteger. In the current mock test, we are mocking the isInteger function.
 
 const isAtLeast18 = require('../isAtLeast18');
+const isInteger = require('../isInteger');
 
 // the mock factory returns the function () => false
-jest.mock('../isInteger', () => () => false);
+// jest.mock('../isInteger', () => () => false);
 
-describe('isAtLeast18', () => {
-	test('fails if value is not recognised as integer', () => {
-		// should pass but fails because of the isInteger mock
-		expect(isAtLeast18(19)).toBe(false);
-		// should fail either way
-		expect(isAtLeast18("ab")).toBe(false);
-	})
+// describe('isAtLeast18', () => {
+// 	test('fails if value is not recognised as integer', () => {
+// 		// should pass but fails because of the isInteger mock
+// 		expect(isAtLeast18(19)).toBe(false);
+// 		// should fail either way
+// 		expect(isAtLeast18("ab")).toBe(false);
+// 	})
+// })
+
+// Another way to implement mock function where implemention can be changed
+jest.mock('../isInteger', () => jest.fn());
+
+describe("isAtLeast18", () => {
+	test('isInteger is mocked to return false', () => {
+		isInteger.mockImplementation(() => false);
+		expect(isAtLeast18(123)).toBe(false);
+		expect(isAtLeast18("abc")).toBe(false);
+	});
+	test('isInteger is mocked to return true', () => {
+		isInteger.mockImplementation(() => true);
+		expect(isAtLeast18(123)).toBe(true);
+		expect(isAtLeast18("abc")).toBe(false);
+	});
 })
